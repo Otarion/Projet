6c09286ada740ebef189d5daad70c76636a1957f
Caro <c.garsault@laposte.net>
2024-05-15 16:21:23 +0200

    Pagination (13/38)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return view ('layouts.default');
+        return view ('layouts.news',[
+            'posts'=> Post::latest()->paginate(10),
+        ]);
     }
 }
